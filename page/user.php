5cd476ac237099ba2a580a019ff2172e91e3d040
Tanel Otsa <tanelotsa@users.noreply.github.com>
2016-11-23 15:50:36 +0200

    interest delete fixed

--- page/user.php
+++ page/user.php
@@ -34,6 +34,11 @@
 		
 	}
 	
+	if(isset($_GET["delete"])){
+    $Interest->deleteInterest($_GET["id"]);
+
+    //exit();
+	}
 
 
     $interests = $Interest->getAllInterests();
@@ -56,35 +61,42 @@
     </div>
 
     <div class="container">
-        <h2><strong>Nimi: </strong><?=$_SESSION["userName"];?></h2>
-        <h2><strong>Sugu:</strong> <?=$_SESSION["userGender"];?></h2>
-        <h2><strong>Elukoht: </strong><?=$_SESSION["userLocation"];?></h2>
-        <h2><strong>Sünnikuupäev:</strong> <?=$_SESSION["userBirthDate"];?></h2>
-    
-	
-	
-	
-	<h2>Salvesta huvi</h2>
+		<div class="col-sm-4 col-md-5">
+			<h2><strong>Nimi: </strong><?=$_SESSION["userName"];?></h2>
+			<h2><strong>Sugu:</strong> <?=$_SESSION["userGender"];?></h2>
+			<h2><strong>Elukoht: </strong><?=$_SESSION["userLocation"];?></h2>
+			<h2><strong>Sünnikuupäev:</strong> <?=$_SESSION["userBirthDate"];?></h2>
+		
+		
+		
+		
+		<h2>Salvesta huvi</h2>
 
-	<form method="POST">
-		
-		<label>Hobi/huviala nimi</label><br>
-		<input name="interest" type="text">
-		
-		<input type="submit" value="Salvesta">
-		
-	</form>
-	</div>
-<h2>Arhiiv</h2>
+		<form method="POST">
+			
+			<label>Hobi/huviala nimi</label><br>
+			<input name="interest" type="text">
+			
+			<input type="submit" value="Salvesta">
+			
+		</form>
+		</div>
+	
+	<div class="col-sm-4 col-md-4 col-md-offset-2">
+	
+	
+	
+<h2>Huvialad</h2>
 	
 <?php
 	
 	
-	$html = "<table>";
+	$html = "<table class='table table-bordered table-condensed '>";
 	
 		$html .= "<tr>";
 			//$html .= "<td>ID</td>";
 			$html .= "<td>Huviala</td>";
+			$html .= "<td>Kustuta</td>";
 		$html .= "</tr>";
 		
 		foreach ($interests as $i) {
@@ -92,9 +104,9 @@
 			$html .= "<tr>";
 				//$html .= "<td>".$s->id."</td>";
 				$html .= "<td>".$i->interest."</td>";
-				$html .= "<td><a href='edit.php?id=".$i->id."'>edit.php</a></td>";
+				$html .= "<td> <a class='btn btn-danger btn-xs' href='user.php?id=".$i->id."&delete=true'> <span class='glyphicon glyphicon-remove'></span></a></td>";
 			$html .= "</tr>";
-			
+
 		}
 		
 	$html .= "</table>";
@@ -103,6 +115,6 @@
 	
 	
 ?>
-
-
+	</div>
+	</div>
 <?php require("../footer.php"); ?>
